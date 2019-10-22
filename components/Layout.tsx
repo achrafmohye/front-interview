a26e1412aca2330ae919cba3b4c4d9039744c765
a.mohyeddine <a.mohyeddine@gmail.com>
2019-10-22 15:51:33 +0200

    seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import {StyledContainer, StyledContent} from './layout.styled';
+import React from "react";
+import { StyledContainer, StyledContent } from "./layout.styled";
+import Head from "next/head";
 
-import Footer from './Footer'
-import Header from './Header'
+import Footer from "./Footer";
+import Header from "./Header";
 
-import GlobalStyle from './theme';
-
+import GlobalStyle from "./theme";
 
 export interface Props {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<Props> = ({children}) => {
+const Layout: React.FC<Props> = ({ children }) => {
   return (
     <StyledContainer>
-      <GlobalStyle/>
+      <GlobalStyle />
       <StyledContent>
-        <Header/>
+        <Header />
+        <Head>
+          <title>Coding test with Nextjs</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         {children}
       </StyledContent>
-      <Footer/>
+      <Footer />
     </StyledContainer>
-  )
-}
+  );
+};
 
 export default Layout;
